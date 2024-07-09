fa92dc0514b3a3049e8dad7a07696d861ee42d07
Punit Sharma <50194646+supersharmapunit@users.noreply.github.com>
2024-07-10 00:48:22 +0530

    code corrections with some gyaan

--- src/main/java/com/milindmishra/todo/Service/service.java
+++ src/main/java/com/milindmishra/todo/Service/service.java
@@ -7,12 +7,13 @@ import com.milindmishra.todo.Entity.Todo;
 @org.springframework.stereotype.Service
 public class Service {
   @Autowired
-  private Repository Repos; 
+  private Repository repository; 
 
     public Todo create(Todo todo) {
         // Todo newTodo = Repos.save(todo);
-        Todo newTodo = new Todo();
-        newTodo.save(null);
+        // Todo newTodo = new Todo(); no need to do this
+        // newTodo.save(null); -> this is not a Todo's fn
+        repository.save(todo);
         return newTodo;
     }
 }
